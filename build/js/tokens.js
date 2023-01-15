d43ed3ff3d697fd980ac7baee757aca1cc2c8dd8
Rodrigo <rodrigorael53@gmail.com>
2023-01-15 15:02:11 -0300

    feat: create the figma tokens in js and css

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 15 Jan 2023 17:58:51 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#885cf6"
+};
